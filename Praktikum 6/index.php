cf6b85975fc5f1af7f4ad8a6e5f6a7e883aee79b
firylkh <khalilahfiryal@gmail.com>
2024-03-27 11:12:58 +0700

    PRAKTIKUM 6

---
+++ Praktikum 6/index.php
@@ -0,0 +1,26 @@
+  <?php
+    include_once('top.php');
+    include_once('menu.php');
+    ?>
+  <main>
+      <div class="container-fluid px-4">
+          <h3 class="mt-4">Daftar Pasien</h3>
+
+          <table class="table mt-4">
+            <tr>
+              <th>No</th>
+              <th>Nama</th>
+              <th>Tempat Lahir</th>
+              <th>Tanggal Lahir</th>
+              <th>JK</th>
+              <th>Alamat</th>
+              <th>Opsi</th>
+            </tr>
+          </table>
+      </div>
+  </main>
+
+
+  <?php
+    include_once('buttom.php')
+    ?>
